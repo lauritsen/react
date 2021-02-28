a91c5443c630020efe11e7fb0947bdf3beab21c2
Henrik Lauritsen <lauritsen@gmail.com>
2021-02-28 16:54:55 +0100

    Using React.createRef to read input field

--- github-cards-app/src/Form.js
+++ github-cards-app/src/Form.js
@@ -1,10 +1,20 @@
 import React from 'react';
 
 class Form extends React.Component {
+  userNameInput = React.createRef();
+  handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(this.userNameInput.current.value);
+  };
   render() {
     return (
-      <form action=''>
-        <input type='text' placeholder='GitHub username' />
+      <form onSubmit={this.handleSubmit}>
+        <input
+          type='text'
+          placeholder='GitHub username'
+          ref={this.userNameInput}
+          required
+        />
         <button>Add card</button>
       </form>
     );
