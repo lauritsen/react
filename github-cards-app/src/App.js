18236bd4f2ab56824fbed41d23744afd981db9df
Henrik Lauritsen <lauritsen@gmail.com>
2021-02-28 15:44:24 +0100

    Refactor to class component

--- github-cards-app/src/App.js
+++ github-cards-app/src/App.js
@@ -1,14 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className='App'>
-      <h1>The GitHub Cards App</h1>
+class App extends React.Component {
+  render() {
+    return (
+      <div className='App'>
+        <h1>The GitHub Cards App</h1>
 
-      <img src={logo} className='App-logo' alt='logo' />
-    </div>
-  );
+        <img src={logo} className='App-logo' alt='logo' />
+      </div>
+    );
+  }
 }
 
 export default App;
